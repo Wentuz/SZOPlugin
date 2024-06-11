b54cf27e18d3fd6e449ca7d55ddadd5fdc82382d
Wentuziak <gabrielurbanski@wp.pl>
2024-06-11 15:28:24 +0200

    defeather chicken

--- szoplugin/src/main/java/wentuziak/szoplugin/EntityListener.java
+++ szoplugin/src/main/java/wentuziak/szoplugin/EntityListener.java
@@ -1,19 +1,37 @@
 package wentuziak.szoplugin;
-
+import org.bukkit.Material;
 import org.bukkit.entity.Cow;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Skeleton;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class EntityListener implements Listener {
 
     @EventHandler
     public void onEntityRightClick(PlayerInteractEntityEvent event)
     {
+        //System.out.println("Right clicked: " + event.getRightClicked().getClass());
+
         if (event.getRightClicked() instanceof Cow) {
             Cow cow = (Cow) event.getRightClicked();
 
             cow.getWorld().createExplosion(cow.getLocation(), 2.5F);
+        }
+        if (event.getRightClicked().getType() == EntityType.CHICKEN) {
+            LivingEntity chickenClicked = (LivingEntity) event.getRightClicked();
+            ItemStack itemToDrop = new ItemStack(Material.FEATHER);
+            double damageAmount = 2.0;
+
+
+            chickenClicked.damage(damageAmount);
+            if (!chickenClicked.isDead()) {
+                chickenClicked.getWorld().dropItem(chickenClicked.getLocation(), itemToDrop);
+            }
         }
     }
 }
