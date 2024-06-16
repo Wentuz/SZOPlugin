30582681c16ab5e165a4f48c4042a88d8d210055
Wentuziak <gabrielurbanski@wp.pl>
2024-06-16 15:13:49 +0200

    ChargeJumper

--- szoplugin/src/main/java/wentuziak/szoplugin/InteractionListener.java
+++ szoplugin/src/main/java/wentuziak/szoplugin/InteractionListener.java
@@ -6,6 +6,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 
 
@@ -18,11 +20,21 @@ public class InteractionListener implements Listener{
     {
         Player player = event.getPlayer();
         ItemStack itemInHand = player.getInventory().getItemInMainHand();
+        //Vector lookDirection = player.getLocation().getDirection();
   
         System.out.println("RIGHT CLICKED: " + itemInHand);
+        //System.out.println("LOOK DIRECTION: " + lookDirection);
 
-        if (itemInHand.getType() == Material.FEATHER) {
-            player.setGravity(true);
+
+        if (itemInHand.getType() == Material.FIRE_CHARGE) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 20, 2));
+            player.getWorld().createExplosion(player.getLocation(), 4F, false, false);
+
+            if (itemInHand.getAmount() > 1) {
+                itemInHand.setAmount(itemInHand.getAmount() - 1);
+            } else {
+                player.getInventory().removeItem(itemInHand);
+            }
         }
     }
 
