ab887c240190f4c4b38426a8828e79447d6e34ac
Wentuziak <gabrielurbanski@wp.pl>
2024-06-18 16:57:28 +0200

    Explosive sword -- early implementation

--- szoplugin/src/main/java/wentuziak/szoplugin/InteractionListener.java
+++ szoplugin/src/main/java/wentuziak/szoplugin/InteractionListener.java
@@ -1,9 +1,14 @@
 package wentuziak.szoplugin;
 
+import javax.swing.Action;
+import javax.swing.text.html.parser.Entity;
+
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
@@ -22,6 +27,7 @@ public class InteractionListener implements Listener{
         Player player = event.getPlayer();
         ItemStack itemInHand = player.getInventory().getItemInMainHand();
         boolean isPerishable = false;
+
   
         //System.out.println("RIGHT CLICKED: " + itemInHand);
         //System.out.println("LOOK DIRECTION: " + lookDirection);
@@ -41,9 +47,17 @@ public class InteractionListener implements Listener{
             givePotionEffect(player, "FIRE_RESISTANCE", 400, 0);
             givePotionEffect(player, "POISON", 200, 1);
 
+            player.playSound(player.getLocation(), Sound.ENTITY_MAGMA_CUBE_SQUISH, 10, 10);
             isPerishable = true;
         }
 
+        if (itemInHand.getType() == Material.PHANTOM_MEMBRANE) {
+            givePotionEffect(player, "SLOW_FALLING", 200, 0);
+           
+            player.playSound(player.getLocation(), Sound.ENTITY_PHANTOM_HURT, 10, 10);
+            isPerishable = true;
+        }
+        
 
         //
         //      Remove one time use item form player
@@ -56,6 +70,8 @@ public class InteractionListener implements Listener{
         }
     }
 
+    
+ 
 
     @EventHandler
     public void onPlayerItemConsume(PlayerItemConsumeEvent event)
@@ -73,6 +89,28 @@ public class InteractionListener implements Listener{
     }
 
 
+    @EventHandler
+    public void onEntityDamageEntity(EntityDamageByEntityEvent event)
+    {   
+        int critChance = 0;
+
+        if (event.getDamager() instanceof Player) {
+            Player player = (Player) event.getDamager();
+            ItemStack itemInHand = player.getInventory().getItemInMainHand();
+            org.bukkit.entity.Entity hitEntity = event.getEntity();
+
+            //player.sendMessage("You hit an entity: " + hitEntity + itemInHand);
+
+            if (itemInHand.getType() == Material.GOLDEN_SWORD) {
+                critChance = (int)(Math.random() * 10 + 1);
+                System.out.println(critChance);
+                if (critChance > 7) {
+                    hitEntity.getWorld().createExplosion(hitEntity.getLocation(), 2, false, false);
+                }
+            }
+
+        }
+    }
 
     public void givePotionEffect(Player player,String effect,int duration,int amplifier)
     {
@@ -80,4 +118,5 @@ public class InteractionListener implements Listener{
         player.addPotionEffect(new PotionEffect(typeOfEffect , duration, amplifier));
     }
 
+    
 }
